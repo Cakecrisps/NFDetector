0d46f30a7946e56443b43d4dec36c5b011b9f153
Nestor <nestorkotov71@gmail.com>
2025-12-09 23:08:36 +0300

    AlertClass

---
+++ general/AlarmClass.py
@@ -0,0 +1,43 @@
+import datetime
+from logs import log, log_http
+class Alarm:
+    def __init__(self, reason: str, ipdst: str, ipsrc: str, is_prot: bool):
+        self.reason = reason
+        self.ipdst = ipdst
+        self.ipsrc = ipsrc
+        self.message = f"{datetime.datetime.now()} || {reason} || {ipdst} -> {ipsrc} || {is_prot}"
+    
+    def __str__(self):
+        return self.message
+    
+    def log(self,logfile: str,logtypes: list[str]) -> list[str]:
+        #["PRINT","LOG","POST:example.domain.com"]
+        errors = []
+
+        logtypes = [x.lower() for x in logtypes.copy()]
+        logtypes = set(logtypes)
+
+        if "print" in logtypes:
+            print(self.message)
+            errors.append(False)
+
+        if "log" in logtypes:
+            try:
+                log(self.message,logfile)
+                errors.append(False)
+            except:
+                errors.append(Exception)
+
+        if len(logtypes) > len(errors):
+            try:
+                log_http(self.message,logfile)
+                errors.append(False)
+            except:
+                errors.append(Exception)
+
+
+        
+
+        
+
+
